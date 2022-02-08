c6b6a05471c56abfadd87af00b72859a83fcecb8
Undude <72217015+SashkaPIX@users.noreply.github.com>
2022-02-08 11:17:54 +0300

    Init commit

---
+++ Librotech-Inspection.Desktop/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Librotech_Inspection.Desktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
