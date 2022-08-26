12d60924438b3e5c1ea003b68e4aaf24afbccf00
andude10 <indko.06@gmail.com>
2022-08-26 14:29:20 +0200

    refactor DialogInteractions.cs

--- src/LibrotechInspection.Desktop/Utilities/Interactions/DialogInteractions.cs
+++ src/LibrotechInspection.Desktop/Utilities/Interactions/DialogInteractions.cs
@@ -7,10 +7,10 @@ namespace LibrotechInspection.Desktop.Utilities.Interactions;
 public class DialogInteractions
 {
     /// <summary>
-    ///     ShowOpenFileDialog is used when a file open dialog should be displayed.
-    ///     Output - The path to the file
+    ///     Save IBitmap as a file with the path specified by the user
+    ///     Input - IBitmap image to save, initial file name
     /// </summary>
-    public readonly Interaction<Unit, string?> ShowOpenFileDialog = new();
+    public readonly Interaction<(IBitmap, string), Unit> SaveBitmapAsPng = new();
 
     /// <summary>
     ///     ShowSaveFileDialog is used when a save file dialog should be displayed.
@@ -19,8 +19,8 @@ public class DialogInteractions
     public readonly Interaction<Unit, string?> SaveTextFileDialog = new();
 
     /// <summary>
-    ///     Save IBitmap as a file with the path specified by the user
-    ///     Input - IBitmap image to save, initial file name
+    ///     ShowOpenFileDialog is used when a file open dialog should be displayed.
+    ///     Output - The path to the file
     /// </summary>
-    public readonly Interaction<(IBitmap, string), Unit> SaveBitmapAsPng = new();
+    public readonly Interaction<Unit, string?> ShowOpenFileDialog = new();
 }
